cdc972068dae2621d4e86f2fa443550bd1da3697
Mateusz <mateusz.gibki@gmail.com>
2017-10-03 21:20:31 +0200

    adding new project files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+ $(document).ready(function(){
+        $('.view').click(function(){
+          $('.det').addClass('active')
+        })
+        $('.close').click(function(){
+          $('.det').removeClass('active')
+        })
+      });
